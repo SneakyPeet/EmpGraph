0418a26bc703be619c2d6783bbcbeb87c9a748c7
Pieter Koornhof <pieter@entelect.co.za>
2015-04-10 20:51:38 +0200

    Get File omg

--- src/EmpGraph.Core/EmperorM2mApp.fs
+++ src/EmpGraph.Core/EmperorM2mApp.fs
@@ -1,14 +1,27 @@
 ﻿namespace EmpGraph.Core
 open FSharp.Data
+open System.IO
 
 module emperorM2mApp =
 
-//    let parse
+    type private m2mFile = HtmlProvider<"M2M_TypeProvider.html">
+
+    type M2MDetails = { client:string; id:string; openValue:decimal; closeValue:decimal; deposit:decimal}
+
+    let rec parseRowsToDetails rows details =
+        match rows with
+        | [] -> details
+        | head :: tail -> parseRowsToDetails tail details
+
+    let private parse (file:Stream) =
+        let m2m = m2mFile.Load(file)
+        m2m.Tables.Table1.Rows |> Array.toList |> parseRowsToDetails
 
     let parseZipFileStream stream =
         let streams = 
             stream
-            |>zipHelper.getM2Mfiles
-            |> Seq.toList
+            |>zipHelper.getXlsFiles
+            |> Seq.toList //todo take out
+            |> List.map parse
         errorHandling.Success streams.Length
         
