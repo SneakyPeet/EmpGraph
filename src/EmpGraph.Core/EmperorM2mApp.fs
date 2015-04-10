0a172a55cb9d0b9dab687b6f7367448d23141d2c
Pieter Koornhof <pieter@entelect.co.za>
2015-04-10 21:48:07 +0200

    remove fluff

--- src/EmpGraph.Core/EmperorM2mApp.fs
+++ src/EmpGraph.Core/EmperorM2mApp.fs
@@ -1,21 +1,39 @@
 ﻿namespace EmpGraph.Core
 open FSharp.Data
 open System.IO
+open System.Text.RegularExpressions
 
 module emperorM2mApp =
 
-    type private m2mFile = HtmlProvider<"M2M_TypeProvider.html">
-
     type M2MDetails = { client:string; id:string; openValue:decimal; closeValue:decimal; deposit:decimal}
 
-    let rec parseRowsToDetails rows details =
-        match rows with
-        | [] -> details
-        | head :: tail -> parseRowsToDetails tail details
+    type private m2mFile = HtmlProvider<"M2M_TypeProvider.html">
+    let private bf = "Brought Forward"
+    let private rbf = "BroughtForward"
+    let private cf = "Carried Forward"
+    let private rcf = "CarriedForward"
+    let private ne = "Net equity"
+    let private nowrap = "<td nowrap>"
+    let private removeHtmlPattern = "<[^>]*>"
+    let private noWhiteSpacePattern = @"\s+"
+    let private depositMatch = "\|Deposit\|*\|"
+
+    let clearFluf (file:string) = 
+        let frombf = file.Substring(file.LastIndexOf(bf))
+        let tone = frombf.Substring(0,frombf.IndexOf(ne))
+        let nohtml = Regex.Replace(tone,removeHtmlPattern,"")
+        let replacebf = nohtml.Replace(bf,rbf).Replace(cf,rcf)
+        Regex.Replace(replacebf,noWhiteSpacePattern,"\n")
+
+    let private parseToDetails (content:string) =
+        let details = { client = "" ; id =""; openValue = 0m; closeValue = 0m; deposit = 0m }
+        let clean = clearFluf content
+        let deposit =  Regex.Match(clean, depositMatch)//.Result().Remove("|").Remove("Deposit")
+        0
 
     let private parse (file:Stream) =
-        let m2m = m2mFile.Load(file)
-        m2m.Tables.Table1.Rows |> Array.toList |> parseRowsToDetails
+        use reader = new StreamReader(file)
+        reader.ReadToEnd() |> parseToDetails 
 
     let parseZipFileStream stream =
         let streams = 
