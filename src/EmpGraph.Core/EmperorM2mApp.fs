41b48c46f07717154cabcc07c30b219f82eacd65
Pieter Koornhof <pieter@entelect.co.za>
2015-04-10 22:13:26 +0200

    Extract Values

--- src/EmpGraph.Core/EmperorM2mApp.fs
+++ src/EmpGraph.Core/EmperorM2mApp.fs
@@ -5,31 +5,48 @@ open System.Text.RegularExpressions
 
 module emperorM2mApp =
 
-    type M2MDetails = { client:string; id:string; openValue:decimal; closeValue:decimal; deposit:decimal}
+    type M2MDetails = { client:string; id:string; openValue:string; closeValue:string; deposit:string}
 
-    type private m2mFile = HtmlProvider<"M2M_TypeProvider.html">
     let private bf = "Brought Forward"
     let private rbf = "BroughtForward"
     let private cf = "Carried Forward"
     let private rcf = "CarriedForward"
+    let private dp = "Deposit"
     let private ne = "Net equity"
     let private nowrap = "<td nowrap>"
     let private removeHtmlPattern = "<[^>]*>"
     let private noWhiteSpacePattern = @"\s+"
-    let private depositMatch = "\|Deposit\|*\|"
 
     let clearFluf (file:string) = 
         let frombf = file.Substring(file.LastIndexOf(bf))
         let tone = frombf.Substring(0,frombf.IndexOf(ne))
         let nohtml = Regex.Replace(tone,removeHtmlPattern,"")
         let replacebf = nohtml.Replace(bf,rbf).Replace(cf,rcf)
-        Regex.Replace(replacebf,noWhiteSpacePattern,"\n")
+        Regex.Replace(replacebf,noWhiteSpacePattern,"|")
+
+    let rec getDetail (details:M2MDetails) lines =
+        match lines with
+        | [] -> details
+        | head::tail -> 
+            match head with
+            | "BroughtForward" -> 
+                let n = { details with openValue = tail.Head }
+                getDetail n tail
+            | "Deposit" -> 
+                let n = { details with deposit = tail.Head }
+                getDetail n tail
+            | "CarriedForward" ->
+                let n = { details with closeValue = tail.Head }
+                getDetail n tail
+            | _ -> getDetail details tail
 
     let private parseToDetails (content:string) =
-        let details = { client = "" ; id =""; openValue = 0m; closeValue = 0m; deposit = 0m }
-        let clean = clearFluf content
-        let deposit =  Regex.Match(clean, depositMatch)//.Result().Remove("|").Remove("Deposit")
-        0
+        let details = { client = "" ; id =""; openValue = ""; closeValue = ""; deposit = "" }
+        (clearFluf content).Split('|')
+        |> Array.toList
+        |> getDetail details
+        
+        
 
     let private parse (file:Stream) =
         use reader = new StreamReader(file)
